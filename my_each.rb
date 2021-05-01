50f85dc9a6471c280ffbe26bf85b678e4dfbd12a
jonathon McClendon <jonathonsmcclendon@gmail.com>
2021-05-01 20:08:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each (array)
   
   if block_given?
-  i = 0
-  
-  while i < array.length
-    yield array[i]
-    i += 1
-  end
+    i = 0
+    
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
   
   else
     puts "no block was given (#my_each)"
